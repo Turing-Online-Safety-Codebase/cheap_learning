618f7ea6592d0a08f712627671696b5a35cb95bb
Hannah Rose Kirk <hannah@thekirks.co.uk>
2022-11-07 11:43:59 +0000

    Add script docstring, edit trailing white space

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,15 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+Evaluates prediction array.
+"""
+
 from sklearn.metrics import (
-    accuracy_score, 
-    f1_score, 
-    precision_score, 
-    recall_score, 
+    accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score,
     confusion_matrix,
-    classification_report, 
+    classification_report,
     )
 
 # # example
 # y_true = [1, 1, 0, 1]
-# y_pred = [1, 1, 0, 0] 
+# y_pred = [1, 1, 0, 0]
 # result = evaluation(y_true, y_pred)
 # print(result)
 
@@ -31,8 +38,6 @@ def evaluation(true, pred, target_names=["hateful", "not_hateful"]):
     results['f1'] = f1_score(true, pred, average='weighted', zero_division=0)
     print(f'--confusion metric-- \n {confusion_matrix(true, pred)}')
     results['tn, fp, fn, tp'] = confusion_matrix(true, pred, normalize='true').ravel()
-
     print("\n--full report--")
     print(classification_report(true, pred, output_dict=False, target_names=target_names))
     return results
-
