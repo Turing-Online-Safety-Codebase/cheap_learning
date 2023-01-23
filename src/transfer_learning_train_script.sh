cf4a0929c56bc9a074cc3b4a294860f7f2c98ef9
“HannahKirk” <hannah@thekirks.co.uk>
2023-01-23 10:06:21 +0000

    Edit transfer learning scripts

--- src/transfer_learning_train_script.sh
+++ src/transfer_learning_train_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "serialised training with different training size"
+echo "transfer learning: serialised training with different training size"
 python src/transfer_learning.py --n_train_values "16,32" --balanced_train 0 --model_name "distilbert-base-cased" --task "binary_abuse" 
