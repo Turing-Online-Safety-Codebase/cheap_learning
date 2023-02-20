46b09df5bc430c637ccbb652ab25f33881230cbe
Angus Williams <arwilliams@turing.ac.uk>
2023-02-20 13:46:34 +0000

    Update dev_sampler to parse task as argument

--- src/dev_sampler.py
+++ src/dev_sampler.py
@@ -4,7 +4,7 @@
 """
 Loads full dev set from file and creates a stratified 10% sample for technique development.
 """
-
+import sys
 import os
 import pandas as pd
 import numpy as np
@@ -27,25 +27,34 @@ from sklearn.model_selection import train_test_split
 #     return df_
 
 def main():
+    task = sys.argv[1] # binary_abuse or binary_movie_sentiment
+    if task=="binary_abuse": x_cols = ['rev_id','text','split']
+    elif task=="binary_movie_sentiment": x_cols = ['text','split']
+    else: raise Exception(f"Invalid task {task}")
+
     SEED = 123
+
     # set directories
-    task = 'binary_abuse' # Note: could put this as arg to script.
     main_dir = os.getcwd()
     print(f'Current working directory is: {main_dir}')
     data_dir = f'{main_dir}/data'
+
     # Load dev data
     orig_dev = pd.read_csv(f'{data_dir}/{task}/clean_data/{task}_dev.csv')
     print(f"Original dev set size: {len(orig_dev)}")
     print(f"Original value counts:\n{orig_dev['label'].value_counts(normalize = True)}")
+
     # Take 10% sample stratified by class labels
-    X = orig_dev[['rev_id', 'text', 'split']]
+    X = orig_dev[x_cols]
     Y = np.array(orig_dev['label'])
     unsampled_X, sample_X, unsampled_Y, sample_Y = train_test_split(X, Y, test_size = 0.1, stratify = Y, random_state = SEED)
+    
     # Recombine as dataframe
     sample_dev = sample_X.copy()
     sample_dev['label'] = sample_Y.tolist()
     print(f"Sampled dev set size: {len(sample_dev)}")
     print(f"Sampled value counts:\n{sample_dev['label'].value_counts(normalize = True)}")
+    
     # Save
     save_dir = f"{data_dir}/{task}/clean_data"
     sample_dev.to_csv(f'{save_dir}/{task}_dev_sample.csv', encoding = 'utf-8', index = True)
